45409b86edd67f968f1c7d2b23fb4801d8f5f576
Aditi <singhaditi.as75@gmail.com>
2026-02-06 16:39:49 +0530

    first program in the class

---
+++ JAVASCRIPT/BASICS/index.js
@@ -0,0 +1,23 @@
+var a=10;
+let b=20;
+const c=30;
+
+
+d=40;// default variables 
+console.log(typeof(b))
+console.log(typeof(d));
+
+// data types 
+
+let num=100;
+let str="hello world";
+let und;
+let nul=null;
+let bool =true;
+
+console.log(typeof(num))
+console.log(typeof(str));
+console.log(typeof(und))
+console.log(typeof(nul));
+console.log(typeof(bool));
+
